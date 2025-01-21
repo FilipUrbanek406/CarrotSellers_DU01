f6e41ebe7ced0cb2ec4610683f0bed951b7cdea0
FilipUrbanek406 <filipurbanek406@gmail.com>
2025-01-21 13:49:09 +0100

    Initial commit

---
+++ src/com/FilipUrbanek406/CarrotSeller.java
@@ -0,0 +1,13 @@
+package com.FilipUrbanek406;
+
+import java.time.LocalDate;
+
+public class CarrotSeller1 {
+    private String name;
+    private LocalDate dateOfBirth;
+    private int signedContracts;
+    private int weightOfTuns;
+    private String city;
+    private String SPZ;
+    private double fuelConsumption;
+}
