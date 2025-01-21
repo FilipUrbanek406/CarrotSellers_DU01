a5ea8aedbf9ba15ef4f7c21957a3dedd07bbc7b2
FilipUrbanek406 <filipurbanek406@gmail.com>
2025-01-21 13:50:22 +0100

    Commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,31 @@
+import com.FilipUrbanek406.CarrotSeller;
+
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+
+        CarrotSeller carrotSeller1 = new CarrotSeller();
+        CarrotSeller carrotSeller2 = new CarrotSeller();
+
+        carrotSeller1.setName("Karel Novák");
+        carrotSeller1.setDateOfBirth(LocalDate.of(1990, 10,10));
+        carrotSeller1.setSignedContracts(100);
+        carrotSeller1.setWeightOfTuns(12);
+        carrotSeller1.setCity("Praha");
+        carrotSeller1.setSPZ("2A4 2896");
+        carrotSeller1.setFuelConsumption(7.5);
+
+        carrotSeller2.setName("Petr Dvořák ");
+        carrotSeller2.setDateOfBirth(LocalDate.of(1998, 9,11));
+        carrotSeller2.setSignedContracts(50);
+        carrotSeller2.setWeightOfTuns(6);
+        carrotSeller2.setCity("Brno");
+        carrotSeller2.setSPZ("2B4 5246");
+        carrotSeller2.setFuelConsumption(6.7);
+
+        System.out.println("Jméno prvního prodejce " + carrotSeller1.getName());
+        System.out.println("Jméno druhého prodejce " + carrotSeller2.getName());
+
     }
 }
--- src/com/FilipUrbanek406/CarrotSeller.java
+++ src/com/FilipUrbanek406/CarrotSeller.java
@@ -2,7 +2,7 @@ package com.FilipUrbanek406;
 
 import java.time.LocalDate;
 
-public class CarrotSeller1 {
+public class CarrotSeller {
     private String name;
     private LocalDate dateOfBirth;
     private int signedContracts;
@@ -10,4 +10,60 @@ public class CarrotSeller1 {
     private String city;
     private String SPZ;
     private double fuelConsumption;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public LocalDate getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(LocalDate dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public int getSignedContracts() {
+        return signedContracts;
+    }
+
+    public void setSignedContracts(int signedContracts) {
+        this.signedContracts = signedContracts;
+    }
+
+    public int getWeightOfTuns() {
+        return weightOfTuns;
+    }
+
+    public void setWeightOfTuns(int weightOfTuns) {
+        this.weightOfTuns = weightOfTuns;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getSPZ() {
+        return SPZ;
+    }
+
+    public void setSPZ(String SPZ) {
+        this.SPZ = SPZ;
+    }
+
+    public double getFuelConsumption() {
+        return fuelConsumption;
+    }
+
+    public void setFuelConsumption(double fuelConsumption) {
+        this.fuelConsumption = fuelConsumption;
+    }
 }
